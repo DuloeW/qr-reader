c5d18954fc7b62d1fc0f2e7482996da66e4336c3
Bayu <91319086+DuloeW@users.noreply.github.com>
2024-01-07 21:20:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react'
+
+const App = () => {
+
+  useEffect(() => {
+    startDetection()
+  }, [])
+
+  const startDetection = async () => {
+    alert("Start detection")
+
+    let video = document.getElementById("barcode-detection")
+    video.srcObject = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+
+    let barcodeDetector = new BarcodeDetector({ format: ["qr_code"] })
+
+    setTimeout(async () => {
+      let canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+
+      let context = canvas.getContext('2d');
+
+      let checkForQrCode = async function () {
+        console.log("Checking for QR code");
+
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        let barcodes = await barcodeDetector.detect(canvas);
+
+        if (barcodes.length > 0) {
+          let barcodeData = barcodes[0].rawValue;
+          alert("Detected barcode " + barcodeData);
+        }
+      };
+      await checkForQrCode();
+    }, 1000);
+  }
+
+
+  return (
+    <div>
+      <video id='barcode-detection'></video>
+    </div>
+  )
+}
+
+export default App
