211db6d7713dbdfe4660078786e295e84018601d
Bayu <91319086+DuloeW@users.noreply.github.com>
2024-01-07 22:04:57 +0800

    Fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,10 @@ const App = () => {
 
     const success = (result) => {
       console.log(result)
+      document.getElementById("result").innerHTML = `
+      <h2>Success</h2>
+      <p><a href="${result}">${result}</a></p>
+      `
       scanner.clear()
     }
 
@@ -30,46 +34,11 @@ const App = () => {
 
   }
 
-  // const startDetection = async () => {
-  //   alert("Start detection")
-
-  //   let video = document.getElementById("barcode-detection")
-  //   video.srcObject = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-
-  //   let barcodeDetector = new BarcodeDetector({ format: ["ean_13"] })
-
-  //   setTimeout(async () => {
-  //     let canvas = document.createElement('canvas');
-  //     canvas.width = video.videoWidth;
-  //     canvas.height = video.videoHeight;
-
-  //     let context = canvas.getContext('2d');
-
-  //     let checkForQrCode = async function () {
-  //       console.log("Checking for QR code");
-
-  //       context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-  //       let barcodes = await barcodeDetector.detect(canvas);
-
-  //       if (barcodes.length > 0) {
-  //         let barcodeData = barcodes[0].rawValue;
-  //         alert("Detected barcode " + barcodeData);
-  //       }
-
-  //       // Check again in 10ms
-  //       setTimeout(() => requestAnimationFrame(checkForQrCode), 10);
-  //     };
-
-  //     await checkForQrCode();
-  //   }, 1000);
-  // }
-
-
   return (
     <div>
       {/* <video id='barcode-detection'></video> */}
-      <div id='barcode-detection' style={{width: 200}}></div>
+      <div id='barcode-detection' style={{width: 400}}></div>
+      <div id="result"></div>
     </div>
   )
 }
