411b54b4c1fede3292ae528ff24b1d4f62450295
Bayu <91319086+DuloeW@users.noreply.github.com>
2024-01-10 13:12:11 +0800

    Tes gak pakai pakage

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import React, { useEffect } from 'react'
 const App = () => {
 
   useEffect(() => {
-    startDetection()
+    // startDetection()
+    startDetection2()
   }, [])
 
   const startDetection = () => {
@@ -34,9 +35,45 @@ const App = () => {
 
   }
 
+  const startDetection2 = async () => {
+    console.log("Starting detection");
+
+    let video = document.getElementById('barcode-detection-video');
+    video.srcObject = await navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}});
+    await video.play();
+
+    let barcodeDetector = new BarcodeDetector({formats: ["qr_code"]});
+
+    setTimeout(async () => {
+        let canvas = document.createElement('canvas');
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+
+        let context = canvas.getContext('2d');
+
+        let checkForQrCode = async function () {
+            console.log("Checking for QR code");
+
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+            let barcodes = await barcodeDetector.detect(canvas);
+
+            if (barcodes.length > 0) {
+                let barcodeData = barcodes[0].rawValue;
+                alert("Detected barcode " + barcodeData);
+            }
+
+            // Check again in 10ms
+            setTimeout(() => requestAnimationFrame(checkForQrCode), 10);
+        };
+
+        await checkForQrCode();
+    }, 1000);
+}
+
   return (
     <div>
-      {/* <video id='barcode-detection'></video> */}
+      <video id='barcode-detection-video' style={{width: "100%"}}></video>
       <div id='barcode-detection' style={{width: 400}}></div>
       <div id="result"></div>
     </div>
